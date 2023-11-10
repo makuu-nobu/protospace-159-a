756598d6e8c1f20c35943e6a8e994d3c5f593939
mikuto17 <ishi17hara@gmail.com>
2023-11-10 16:55:10 +0900

    マイページの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'protospaces#index'
-  resources :protospaces, only: [:index, :new, :create]
+  resources :protospaces, only: [:index, :new, :create, :show]
+  resources :users, only: [:show]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_11_10_031833) do
-  create_table "prototypes", charset: "utf8", force: :cascade do |t|
+  create_table "prototypes", charset: "utf8mb4", force: :cascade do |t|
     t.string "title", null: false
     t.text "catch_copy", null: false
     t.text "concept", null: false
@@ -20,7 +20,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_10_031833) do
     t.integer "user_id"
   end
 
-  create_table "users", charset: "utf8", force: :cascade do |t|
+  create_table "users", charset: "utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
