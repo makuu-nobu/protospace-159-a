ff8eb7f5773aa449a2753fb8eb759f2a10a586b6
makuu-nobu <nobuatsu.ltnl@icloud.com>
2023-11-10 11:36:36 +0900

    devise新規登録とトップページのレイアウト表記の変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_09_093436) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_10_015951) do
   create_table "prototypes", charset: "utf8", force: :cascade do |t|
     t.string "title", null: false
     t.text "catch_copy", null: false
@@ -27,6 +27,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_09_093436) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.text "profile"
+    t.text "occupation"
+    t.text "position"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
